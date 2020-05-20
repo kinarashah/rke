e6725a2ed1383b33df0439383f467e3f9aa511a2
kinarashah <kinara@rancher.com>
2020-05-19 21:49:12 -0700

    temp changes

--- templates/templates.go
+++ templates/templates.go
@@ -2,9 +2,14 @@ package templates
 
 import (
 	"bytes"
+	"encoding/json"
+	"strings"
 	"text/template"
 
+	"github.com/Masterminds/sprig"
+	"github.com/ghodss/yaml"
 	"github.com/rancher/rke/util"
+	"github.com/sirupsen/logrus"
 )
 
 var VersionedTemplate = map[string]map[string]string{
@@ -28,6 +33,8 @@ var VersionedTemplate = map[string]map[string]string{
 
 func CompileTemplateFromMap(tmplt string, configMap interface{}) (string, error) {
 	out := new(bytes.Buffer)
+	templateFuncMap := sprig.TxtFuncMap()
+	templateFuncMap["toYaml"] = ToYAML
 	t := template.Must(template.New("compiled_template").Parse(tmplt))
 	if err := t.Execute(out, configMap); err != nil {
 		return "", err
@@ -42,4 +49,20 @@ func GetVersionedTemplates(templateName string, k8sVersion string) string {
 		return t
 	}
 	return versionedTemplate["default"]
+}
+
+func ToYAML(v interface{}) string {
+	data, err := json.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template so it doesn't affect remaining template lines
+		logrus.Errorf("[ToYAML] Error marshaling %v: %v", v, err)
+		return ""
+	}
+	yamlData, err := yaml.JSONToYAML(data)
+	if err != nil {
+		// Swallow errors inside of a template so it doesn't affect remaining template lines
+		logrus.Errorf("[ToYAML] Error converting json to yaml for %v: %v ", string(data), err)
+		return ""
+	}
+	return strings.TrimSuffix(string(yamlData), "\n")
 }
