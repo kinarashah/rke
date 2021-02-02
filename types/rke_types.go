566dea82a9115dafc0d4b98d3fabfca93e23de62
kinarashah <kinara@rancher.com>
2021-02-02 14:53:47 -0800

    add types

--- types/rke_types.go
+++ types/rke_types.go
@@ -929,6 +929,8 @@ type DNSConfig struct {
 	StubDomains map[string][]string `yaml:"stubdomains" json:"stubdomains,omitempty"`
 	// NodeSelector key pair
 	NodeSelector map[string]string `yaml:"node_selector" json:"nodeSelector,omitempty"`
+	// KeySelector
+	KeySelector map[string]string `yaml:"key_selector" json:"keySelector,omitempty"`
 	// Nodelocal DNS
 	Nodelocal *Nodelocal `yaml:"nodelocal" json:"nodelocal,omitempy"`
 	// Update strategy
